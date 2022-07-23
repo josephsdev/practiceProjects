18ae51d6dca58b2afc3c45e0b007e17239703cf7
josephsdev <joseph@sdev.ca>
2022-07-23 12:54:26 -0700

    added comments

--- whaleTranslate.js
+++ whaleTranslate.js
@@ -1,3 +1,4 @@
+// Sentence you would like to translate. Whales do not use consonants and double E's and U's
 let input = "I am looking for directions to the pacific ocean preferably avoiding buoys"
 const vowels = ['a', 'e', 'i', 'o', 'u']
 let resultArray = []
@@ -9,13 +10,13 @@ for(let i = 0; i < input.length; i++){
    if (input[i] === 'u'){
         resultArray.push(input[i])
       }
+      // ^ doubles E's and U's
   for(let x = 0; x < vowels.length; x++){
     if (input[i] === vowels[x]){
       resultArray.push(input[i])
-      
-      
     }
+    // ^ Finds vowels and adds them to resultArray
   }
 }
-let resultSting = resultArray.join('').toUpperCase()
+let resultSting = resultArray.join('').toUpperCase() // Turns result array into string
 console.log(resultSting)
